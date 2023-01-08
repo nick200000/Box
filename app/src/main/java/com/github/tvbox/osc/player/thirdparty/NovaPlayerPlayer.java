73c19464196d31cf89231eef0ee49fa114a87038
nick200000 <119643200+nick200000@users.noreply.github.com>
2023-01-08 14:55:02 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/tvbox/osc/player/thirdparty/NovaPlayerPlayer.java
@@ -0,0 +1,88 @@
+package com.github.tvbox.osc.player.thirdparty;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.util.Log;
+
+import com.github.tvbox.osc.base.App;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class NovaPlayerPlayer {
+    public static final String TAG = "ThirdParty.NovaPlayer";
+
+    private static final String PACKAGE_NAME = "org.courville.nova";
+    private static final String PLAYBACK_ACTIVITY = "org.courville.nova.MainActivity";
+
+    private static class NovaPlayerPackageInfo {
+        final String packageName;
+        final String activityName;
+
+        NovaPlayerPackageInfo(String packageName, String activityName) {
+            this.packageName = packageName;
+            this.activityName = activityName;
+        }
+    }
+
+    private static final NovaPlayerPackageInfo[] PACKAGES = {
+            new NovaPlayerPackageInfo(PACKAGE_NAME, PLAYBACK_ACTIVITY),
+    };
+
+    public static NovaPlayerPackageInfo getPackageInfo() {
+        for (NovaPlayerPackageInfo pkg : PACKAGES) {
+            try {
+                ApplicationInfo info = App.getInstance().getPackageManager().getApplicationInfo(pkg.packageName, 0);
+                if (info.enabled)
+                    return pkg;
+                else
+                    Log.v(TAG, "NovaPlayer Player package `" + pkg.packageName + "` is disabled.");
+            } catch (PackageManager.NameNotFoundException ex) {
+                Log.v(TAG, "NovaPlayer Player package `" + pkg.packageName + "` does not exist.");
+            }
+        }
+        return null;
+    }
+
+    public static boolean run(Activity activity, String url, String title, String subtitle, HashMap<String, String> headers) {
+        NovaPlayerPackageInfo packageInfo = getPackageInfo();
+        if (packageInfo == null)
+            return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setPackage(packageInfo.packageName);
+        intent.setComponent(new ComponentName(packageInfo.packageName, packageInfo.activityName));
+        intent.setData(Uri.parse(url));
+        intent.putExtra("title", title);
+        intent.putExtra("name", title);
+        intent.putExtra("NovaPlayer.extra.title", title);
+        if (headers != null && headers.size() > 0) {
+            try {
+                JSONObject json = new JSONObject();
+                for (String key : headers.keySet()) {
+                    json.put(key, headers.get(key).trim());
+                }
+                intent.putExtra("NovaPlayer.extra.http_header", json.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (subtitle != null && !subtitle.isEmpty()) {
+            intent.putExtra("NovaPlayer.extra.subtitle", subtitle);
+        }
+        try {
+            activity.startActivity(intent);
+            return true;
+        } catch (ActivityNotFoundException ex) {
+            Log.e(TAG, "Can't run NovaPlayer", ex);
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/github/tvbox/osc/player/thirdparty/VLCPlayer.java
@@ -0,0 +1,88 @@
+package com.github.tvbox.osc.player.thirdparty;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.util.Log;
+
+import com.github.tvbox.osc.base.App;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class VLCPlayer {
+    public static final String TAG = "ThirdParty.VLC";
+
+    private static final String PACKAGE_NAME = "org.videolan.vlc";
+    private static final String PLAYBACK_ACTIVITY = "org.videolan.vlc.MainActivity";
+
+    private static class VLCPackageInfo {
+        final String packageName;
+        final String activityName;
+
+        VLCPackageInfo(String packageName, String activityName) {
+            this.packageName = packageName;
+            this.activityName = activityName;
+        }
+    }
+
+    private static final VLCPackageInfo[] PACKAGES = {
+            new VLCPackageInfo(PACKAGE_NAME, PLAYBACK_ACTIVITY),
+    };
+
+    public static VLCPackageInfo getPackageInfo() {
+        for (VLCPackageInfo pkg : PACKAGES) {
+            try {
+                ApplicationInfo info = App.getInstance().getPackageManager().getApplicationInfo(pkg.packageName, 0);
+                if (info.enabled)
+                    return pkg;
+                else
+                    Log.v(TAG, "VLC Player package `" + pkg.packageName + "` is disabled.");
+            } catch (PackageManager.NameNotFoundException ex) {
+                Log.v(TAG, "VLC Player package `" + pkg.packageName + "` does not exist.");
+            }
+        }
+        return null;
+    }
+
+    public static boolean run(Activity activity, String url, String title, String subtitle, HashMap<String, String> headers) {
+        VLCPackageInfo packageInfo = getPackageInfo();
+        if (packageInfo == null)
+            return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setPackage(packageInfo.packageName);
+        intent.setComponent(new ComponentName(packageInfo.packageName, packageInfo.activityName));
+        intent.setData(Uri.parse(url));
+        intent.putExtra("title", title);
+        intent.putExtra("name", title);
+        intent.putExtra("VLC.extra.title", title);
+        if (headers != null && headers.size() > 0) {
+            try {
+                JSONObject json = new JSONObject();
+                for (String key : headers.keySet()) {
+                    json.put(key, headers.get(key).trim());
+                }
+                intent.putExtra("VLC.extra.http_header", json.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (subtitle != null && !subtitle.isEmpty()) {
+            intent.putExtra("VLC.extra.subtitle", subtitle);
+        }
+        try {
+            activity.startActivity(intent);
+            return true;
+        } catch (ActivityNotFoundException ex) {
+            Log.e(TAG, "Can't run VLC Player", ex);
+            return false;
+        }
+    }
+}
