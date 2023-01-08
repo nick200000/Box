9f37138672750ae6cf1f8b859e17f2f909ea3f6c
nick200000 <119643200+nick200000@users.noreply.github.com>
2023-01-08 15:14:19 +0800

    Update VimuPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/thirdparty/VimuPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/thirdparty/VimuPlayer.java
@@ -16,43 +16,43 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-public class VimuPlayerPlayer {
+public class VimuPlayer {
     public static final String TAG = "ThirdParty.VimuPlayer";
 
     private static final String PACKAGE_NAME = "net.gtvbox.videoplayer";
     private static final String PLAYBACK_ACTIVITY = "net.gtvbox.videoplayer.MainActivity";
 
-    private static class VimuPlayerPackageInfo {
+    private static class VimuPackageInfo {
         final String packageName;
         final String activityName;
 
-        VimuPlayerPackageInfo(String packageName, String activityName) {
+        VimuPackageInfo(String packageName, String activityName) {
             this.packageName = packageName;
             this.activityName = activityName;
         }
     }
 
-    private static final VimuPlayerPackageInfo[] PACKAGES = {
-            new VimuPlayerPackageInfo(PACKAGE_NAME, PLAYBACK_ACTIVITY),
+    private static final VimuPackageInfo[] PACKAGES = {
+            new VimuPackageInfo(PACKAGE_NAME, PLAYBACK_ACTIVITY),
     };
 
     public static VimuPlayerPackageInfo getPackageInfo() {
-        for (VimuPlayerPackageInfo pkg : PACKAGES) {
+        for (VimuPackageInfo pkg : PACKAGES) {
             try {
                 ApplicationInfo info = App.getInstance().getPackageManager().getApplicationInfo(pkg.packageName, 0);
                 if (info.enabled)
                     return pkg;
                 else
-                    Log.v(TAG, "VimuPlayer Player package `" + pkg.packageName + "` is disabled.");
+                    Log.v(TAG, "Vimu Player package `" + pkg.packageName + "` is disabled.");
             } catch (PackageManager.NameNotFoundException ex) {
-                Log.v(TAG, "VimuPlayer Player package `" + pkg.packageName + "` does not exist.");
+                Log.v(TAG, "Vimu Player package `" + pkg.packageName + "` does not exist.");
             }
         }
         return null;
     }
 
     public static boolean run(Activity activity, String url, String title, String subtitle, HashMap<String, String> headers) {
-        VimuPlayerPackageInfo packageInfo = getPackageInfo();
+        VimuPackageInfo packageInfo = getPackageInfo();
         if (packageInfo == null)
             return false;
 
@@ -62,20 +62,20 @@ public class VimuPlayerPlayer {
         intent.setData(Uri.parse(url));
         intent.putExtra("title", title);
         intent.putExtra("name", title);
-        intent.putExtra("VimuPlayer.extra.title", title);
+        intent.putExtra("Vimu.extra.title", title);
         if (headers != null && headers.size() > 0) {
             try {
                 JSONObject json = new JSONObject();
                 for (String key : headers.keySet()) {
                     json.put(key, headers.get(key).trim());
                 }
-                intent.putExtra("VimuPlayer.extra.http_header", json.toString());
+                intent.putExtra("Vimu.extra.http_header", json.toString());
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         if (subtitle != null && !subtitle.isEmpty()) {
-            intent.putExtra("VimuPlayer.extra.subtitle", subtitle);
+            intent.putExtra("Vimu.extra.subtitle", subtitle);
         }
         try {
             activity.startActivity(intent);
