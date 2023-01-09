092a75516e234d5e4c283e6e4b48958d7c65ff0d
nick200000 <119643200+nick200000@users.noreply.github.com>
2023-01-09 09:29:25 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -57,9 +57,7 @@ import com.github.tvbox.osc.player.thirdparty.ReexPlayer;
 import com.github.tvbox.osc.player.thirdparty.nPlayer;
 import com.github.tvbox.osc.player.thirdparty.VLCPlayer;
 import com.github.tvbox.osc.player.thirdparty.JustPlayer;
-import com.github.tvbox.osc.player.thirdparty.NovaPlayer;
 import com.github.tvbox.osc.player.thirdparty.Kodinerds;
-import com.github.tvbox.osc.player.thirdparty.VimuPlayer;
 import com.github.tvbox.osc.ui.adapter.SelectDialogAdapter;
 import com.github.tvbox.osc.ui.dialog.SearchSubtitleDialog;
 import com.github.tvbox.osc.ui.dialog.SelectDialog;
@@ -568,17 +566,7 @@ public class PlayFragment extends BaseLazyFragment {
                                     }
                                     case 16: {
                                         extPlay = true;
-                                        callResult = NovaPlayer.run(requireActivity(), url, playTitle, playSubtitle, headers);
-                                        break;
-                                    }
-                                    case 17: {
-                                        extPlay = true;
                                         callResult = Kodinerds.run(requireActivity(), url, playTitle, playSubtitle, headers);
-                                        break;
-                                    }
-                                    case 18: {
-                                        extPlay = true;
-                                        callResult = VimuPlayer.run(requireActivity(), url, playTitle, playSubtitle, headers);
                                         break;
                                     }
                                 }
